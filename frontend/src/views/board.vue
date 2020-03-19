754c73d55d16ede08e1405c3a98e08fece7768a3
avraham <b.a.avraham@gmail.com>
2020-03-19 14:50:13 +0200

    json

--- frontend/src/views/board.vue
+++ frontend/src/views/board.vue
@@ -1,12 +1,17 @@
 <template>
   <section class="board" v-if="boardData">
-      <task-list v-for="taskList in boardData.taskLists" :key="taskList.id" :task-list-data="taskList">
+      <task-list
+      v-for="taskList in boardData.taskLists"
+      :key="taskList.id"
+      :task-list-data="taskList">
       </task-list>
   </section>
 </template>
 
 <script>
+/* eslint-disable */
 import taskList from '../components/task-list.vue'
+import board from './board.json';
 export default {
   data () {
     return {
@@ -15,7 +20,7 @@ export default {
   },
   created () {
     const id = this.$route.params.id
-
+    this.boardData = board
   },
   components: {
     taskList
