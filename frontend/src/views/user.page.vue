f82be9e3e72618ec2ae08d26cfce5521cc66b52b
Ron Hershkovitz <ron9hm1@gmail.com>
2020-03-20 11:00:48 +0200

    pull for new board push

--- frontend/src/views/user.page.vue
+++ frontend/src/views/user.page.vue
@@ -1,11 +1,17 @@
 <template>
   <div class="user-page">
     <h1>User Page</h1>
-    <h1>{{user}}</h1>
+    <div v-for="board in user.boards" :key="board._id">
+    <h3>board id:{{board._id}}</h3>
+    <h3>name:{{board.name}}</h3>
+    <h3>color:{{board.background}}</h3>
+    </div>
+    <pre>{{user}}</pre>
   </div>
 </template>
 
 <script>
+
 export default {
   name: "user-page",
   data() {
@@ -13,13 +19,9 @@ export default {
       user: this.loggedinUser
     };
   },
-  computed: {
-    loggedinUser() {}
-  },
   created() {
     console.log("this.loggedinUser", this.user);
-    var x = this.$store.getters.loggedinUser;
-    this.user = x;
+    this.user =this.$store.getters.loggedinUser;
   },
   methods: {}
 };
