999df105e0dea5f7c374ada838e1d90658fedef6
avraham <b.a.avraham@gmail.com>
2020-03-24 18:21:19 +0200

    a

--- backend/api/socket/socket.routes.js
+++ backend/api/socket/socket.routes.js
@@ -6,11 +6,9 @@ const emitter = require('../../services/emitter.service');
 function connectSockets(io) {
     io.on('connection', socket => {
 
-
         emitter.on('sendSocket' + socket.id, ({ data, boardId }) => {
             socket.broadcast.to(boardId).emit('change-data', data)
         })
-
 
         socket.on('connect-to-board', channelId => {
             if (socket.board) {
@@ -20,13 +18,5 @@ function connectSockets(io) {
             socket.channelId = channelId;
         });
 
-        socket.on('change board', () => {
-            socket.broadcast.to(socket.channelId).emit('change board')
-        });
-
-        socket.on('change task', task => {
-            // console.log('socket.channelId :', socket.channelId);
-            socket.broadcast.to(socket.channelId).emit('change-task', task)
-        })
     })
 }
