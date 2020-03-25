77a893cfc0ef079bafe730fa8aefe21ba936371c
avraham <b.a.avraham@gmail.com>
2020-03-25 20:43:27 +0200

    scc

---
+++ frontend/src/components/header-cmp.vue
@@ -0,0 +1,45 @@
+<template>
+<header class="header-cmp">
+    <div class="boards-menu">
+        <button class="boards-btn btn">
+            <i class="fab fa-trello boards-btn-icon"></i>Boards
+        </button>
+        <div class="board-search">
+            <input type="search" class="board-search-input btn" aria-label="Board Search" />
+            <i class="fas fa-search search-icon" aria-hidden="true"></i>
+        </div>
+    </div>
+
+    <router-link to="/">
+        <div class="logo">
+            <h1>Jello</h1>
+        </div>
+    </router-link>
+
+    <div class="user-settings">
+        <button class="user-settings-btn btn" aria-label="Notifications">
+            <i class="fas fa-bell" aria-hidden="true"></i>
+        </button>
+        <button class="user-settings-btn btn" aria-label="User Settings">
+            <i class="fas fa-user-circle" aria-hidden="true"></i>
+        </button>
+        <button @click="doLogout" class="user-settings-btn btn" aria-label="User Settings">
+            <i class="fa fa-sign-out-alt"></i>
+        </button>
+    </div>
+</header>
+</template>
+
+<script>
+export default {
+    methods: {
+        async doLogout() {
+            await this.$store.dispatch({ type: "logout" });
+            this.$router.push({ path: "/" });
+        }
+    }
+};
+</script>
+
+<style>
+</style>
--- frontend/src/components/nav-board.vue
+++ frontend/src/components/nav-board.vue
@@ -1,28 +1,17 @@
 <template>
 <nav class="nav-board">
-
-    <section class="board-info-bar">
-
-        <div class="board-controls">
-
-            <button @click="changeTitle" class="board-title btn">
-                <input ref="inputTitle" @blur="saveTitle" @keydown="onKeydownTitle" v-if="editTitleMode" type="text" v-model="titleToChange">
-                <h2 v-else>{{boardData.title}}</h2>
-            </button>
-
-            <button class="star-btn btn" aria-label="Star Board">
-                <i class="far fa-star" aria-hidden="true"></i>
-            </button>
-
-            <button class="personal-btn btn">Personal</button>
-
-            <button class="private-btn btn"><i class="fas fa-briefcase private-btn-icon" aria-hidden="true"></i>Private</button>
-
-        </div>
-
-        <button class="menu-btn btn"><i class="fas fa-ellipsis-h menu-btn-icon" aria-hidden="true"></i>Show Menu</button>
-
-    </section>
+    <div class="board-controls">
+        <button @click="changeTitle" class="board-title btn">
+            <input ref="inputTitle" @blur="saveTitle" @keydown="onKeydownTitle" v-if="editTitleMode" type="text" v-model="titleToChange">
+            <h2 v-else>{{boardData.title}}</h2>
+        </button>
+        <button class="star-btn btn" aria-label="Star Board">
+            <i class="far fa-star" aria-hidden="true"></i>
+        </button>
+        <button class="personal-btn btn">Personal</button>
+        <button class="private-btn btn"><i class="fas fa-briefcase private-btn-icon" aria-hidden="true"></i>Private</button>
+    </div>
+    <button class="menu-btn btn"><i class="fas fa-ellipsis-h menu-btn-icon" aria-hidden="true"></i>Show Menu</button>
 
 </nav>
 </template>
