2b1de828daeaddc08b45c0b25a70cb6c171b3e77
avraham <b.a.avraham@gmail.com>
2020-03-26 02:17:15 +0200

    fix merges

--- frontend/src/components/nav-board.vue
+++ frontend/src/components/nav-board.vue
@@ -1,29 +1,13 @@
 <template>
-  <nav class="nav-board">
-    <section class="board-info-bar">
-      <div class="board-controls">
-        <button @click="changeTitle" class="board-title btn">
-          <input
-            ref="inputTitle"
-            @blur="saveTitle"
-            v-if="editTitleMode"
-            type="text"
-            v-model="titleToChange"
-          />
-          <h2 v-else>{{boardData.title}}</h2>
+<nav class="nav-board">
+    <div class="board-controls">
+        <button @click="changeTitle" class="board-title btn" :class="{action:editTitleMode}">
+            <input ref="inputTitle" @blur="saveTitle" @keydown="onKeydownTitle" v-if="editTitleMode" type="text" :style="styleInputTitle" v-model="titleToChange">
+            <h2 v-else>{{boardData.title}}</h2>
         </button>
 
         <button class="star-btn btn" aria-label="Star Board">
           <i class="far fa-star" aria-hidden="true"></i>
-        </button>
-    </div>
-<div>
-    <button class="menu-btn btn">
-        <i class="fas fa-ellipsis-h menu-btn-icon" aria-hidden="true"></i>Show Menu
-        </button>
-
-        <button class="private-btn btn">
-          <i class="fas fa-briefcase private-btn-icon" aria-hidden="true"></i>Private
         </button>
         <add-members
           @filter="filter"
@@ -31,12 +15,10 @@
           :users="users"
           :currBoard="boardData"
         ></add-members>
-      </div>
+    </div>
+    <button class="menu-btn btn"><i class="fas fa-ellipsis-h menu-btn-icon" aria-hidden="true"></i>Show Menu</button>
 
-      <button class="menu-btn btn">
-        <i class="fas fa-ellipsis-h menu-btn-icon" aria-hidden="true"></i>Show Menu
-      </button>
-    </section>
+
   </nav>
 </template>
 
@@ -76,7 +58,8 @@ export default {
         .then(users => {
             this.getUsersFromStore()
         });
-    },updateBoard(updatedBoard) {
+    },
+    updateBoard(updatedBoard) {
       this.$store.dispatch({ type: "saveBoard", updatedBoard }).then(res => {
         console.log(res);
       });
@@ -85,15 +68,15 @@ export default {
       this.users = this.$store.getters.users;
     }
   },
-
-
-      computed: {
+    computed: {
         styleInputTitle() {
             return {
                 width: this.titleToChange.length + 'ch'
             }
         }
-    },  
+    },
+
+  
   computed: {
     
   },
