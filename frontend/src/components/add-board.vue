3641532c2648011074b4acfbfd097af668c2e2b4
Ron Hershkovitz <ron9hm1@gmail.com>
2020-03-27 11:40:47 +0300

    user page v3

---
+++ frontend/src/components/add-board.vue
@@ -0,0 +1,35 @@
+<template>
+  <section ref="container" class="label-picker">
+    <h3>New Modal</h3>
+   <input type="text" placeholder="">
+  </section>
+</template>
+
+<script>
+export default {
+
+  data() {
+    return {
+      prefsObj: {
+           title: "choose title",
+        style: { background: "#FFFFF" },
+        public: true
+      }
+    };
+  },
+  created() {
+      
+  },
+  methods: {
+    addMemberToTask(user) {
+      let isDuplicated = this.members.find(member => {
+        return member._id === user._id;
+      });
+      if (!isDuplicated) {
+        this.members.push(user);
+        this.$emit("input", this.members);
+      }
+    }
+  }
+};
+</script>
